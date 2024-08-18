81393d6792dc84ec4568519bbc24c3c9b6ed46ce
Maryllian Vieira <104434426+albvieiraa@users.noreply.github.com>
2024-08-18 15:36:43 -0300

    day 04 - print()

---
+++ day04/main.py
@@ -0,0 +1,14 @@
+# Print a history
+
+print("=== Your Adventure Simulator ===")
+print("""You'll be asked a bunch of questions then we'll make you up an amazing story with YOU as the star""")
+print()
+name = input("Your name: ")
+enemy = input("Your worst enemy's name: ")
+superPower = input("Your super power: ")
+print()
+
+print(f"""Our story begins as our hero name approaches a foreboding castle... Suddenly a bolt of lightning striked the ground at the feet of {name}.Our final battle begins!shouts the evil {enemy}, cleary missing the fact that {name} has the power of {superPower} which means they'll win quite easily
+""")
+print()
+print("THE END")
