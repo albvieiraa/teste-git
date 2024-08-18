dd96f689c3d2962a7734d3b6e255472a648dca59
Maryllian Vieira <104434426+albvieiraa@users.noreply.github.com>
2024-08-17 21:08:14 -0300

    desafio 02

---
+++ day02/main.py
@@ -0,0 +1,10 @@
+name = input("What is your name? ")
+food = input("What is your favorite food?")
+music = input("What is your favorite music?")
+location = input("Where do you live?")
+print()
+
+print(f"You are {name}. You're probably hungry for {food}. You're definitely getting your groove on to {music} music. You live in {location}.")
+print()
+
+print("You are amazing! Good to know you!")
