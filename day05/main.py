ebf816d28681c94b8acfac60a0ae0ecdfc9885d1
Maryllian Vieira <104434426+albvieiraa@users.noreply.github.com>
2024-08-19 21:10:04 -0300

    day 05 if else condition

---
+++ day05/main.py
@@ -0,0 +1,21 @@
+print("Lord Of The Rings Character Creator")
+print("-----------------------------------")
+legolas = input("Do you know how to use a bow and arrow? : ")
+if legolas == 'Yes':
+  print("You are Legolas!")
+else:
+  print("You might be another character...")
+
+gimli = input("Do you have a beard? : ")
+if gimli == 'Yes':
+  print("You are Gimli!")
+else:
+  print("Hmm..Okay! Let's try another one!")
+
+aragorn = input("Do you like to fight? : ")
+if aragorn == 'Yes':
+  print("You are Aragorn!")
+else:
+  print("Sorry, maybe you have to look for a Harry Potter Character Creator")
+
+print("THE END")
